4b853129c069e238a8271359c39b44fa8073de65
Hetvin Sakariya <hetvinsakariya@gmail.com>
2026-03-30 00:00:35 +0530

    Update condistional_sta.php

--- condistional_sta.php
+++ condistional_sta.php
@@ -1,7 +1,7 @@
 <?php
-$a=100;
-$b=200;
-$c=300;
+$a=10;
+$b=20;
+$c=30;
 
 if($a>$b && $a>$c)
 {	
@@ -9,11 +9,11 @@ if($a>$b && $a>$c)
 }
 elseif($b>$a && $b>$c)
 {
-	echo($b." is maximum num");	
+	echo($b." is max num");	
 }
 else
 {
-	echo($c." is maximum num");
+	echo($c." is max num");
 }
 
 ?>
